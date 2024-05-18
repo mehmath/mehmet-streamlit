d80de93f3d005a29948e41180bf805580e39b86b
Mehmet Saygılı <mehmathsaygili@gmail.com>
2024-05-18 23:29:59 +0300

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -284,37 +284,4 @@ else:
 
 col3.pyplot(plt)
 
-import multiprocessing
 
-must_reload_page = False
-
-def start_flask():
-    if not hasattr(st, 'already_started_server'):
-        st.already_started_server = True
-        must_reload_page = True
-
-        from flask import Flask
-
-        app = Flask(__name__)
-
-        @app.route('/foo')
-        def serve_foo():
-            return 'This page is served via Flask!'
-
-        app.run(port=8888)
-
-def reload_page():
-    if must_reload_page:
-        must_reload_page = False
-        st.experimental_rerun()
-
-if __name__=='__main__':
-    flask_process = multiprocessing.Process(target=start_flask)
-    reload_process = multiprocessing.Process(target=reload_page)
-    flask_process.start()
-    reload_process.start()
-
-
-# Your normal Streamlit app goes here:
-x = st.slider('Pick a number')
-st.write('You picked:', x)
